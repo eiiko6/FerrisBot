9108851f0c3590998b8febdd6c0409d43ebf0e06
eiiko6 <myeiiko6@gmail.com>
2025-06-13 16:45:42 +0200

    basic discord bot with slash commands registration

---
+++ src/commands/mod.rs
@@ -0,0 +1,9 @@
+pub mod age;
+pub mod register;
+
+use crate::Error;
+use poise::Command;
+
+pub fn get_commands() -> Vec<Command<crate::Data, Error>> {
+    vec![age::age(), register::register()]
+}
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use poise::serenity_prelude as serenity;
+
+mod commands;
+
+pub struct Data;
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Context<'a> = poise::Context<'a, Data, Error>;
+
+pub async fn run() -> Result<(), Error> {
+    let token = std::env::var("DISCORD_TOKEN")?;
+    let intents = serenity::GatewayIntents::non_privileged();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: commands::get_commands(),
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data)
+            })
+        })
+        .build();
+
+    serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await?
+        .start()
+        .await?;
+
+    Ok(())
+}
+#[tokio::main]
+async fn main() {
+    if let Err(e) = run().await {
+        eprintln!("Bot error: {}", e);
+    }
+}
